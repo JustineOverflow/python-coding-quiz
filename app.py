0f069b3ca8e751f2ee8885cf168879c20e360276
Justine Fabre <justinefabre84@gmail.com>
2020-03-21 14:55:03 +0000

    get a random question

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, request
-import json
+import random
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -9,21 +10,50 @@ def index():
     return "Welcome to the coding quiz!"
 
 
-class Question:
-    def __init__(self, question, correctAnswer, otherAnswers):
-        self.question = question
-        self.correctAnswer = correctAnswer
-        self.otherAnswers = otherAnswers
+questionsList = [
+    {
+        "id": 1,
+        "name": "Which of the following is not a programming language?",
+        "answer": "Cobra",
+        "choices": ["Cobra", "Python", "Swift"]
+    }
+]
 
 
-@app.route('/questions')
-def getQuestions():
-    questionsList = [
-        {"name": "Which of the following is not a programming language?",
-         "answer": "Cobra",
-         "choices": ["Python", "Swift"]}]
-    return json.dumps(questionsList)
+@app.route('/quiz')
+def get_quiz():
+    question = random.choice(questionsList)
+    return jsonify(question)
+
+    # question =
+    # question_id = request.args.get('question_id')
+    # return '{}'.format(question)
+
+    # score = 0
+    #
+    # for qItem in questionsList:
+    #     print(qItem.question)
+    #     print("Here are the possible answers:")
+    #     possible = qItem.otherAnswers + [qItem.correctAnswer]
+    #     random.shuffle(possible)
+    #     count = 0
+    #     while count < len(possible):
+    #         print(str(count + 1) + ": " + possible[count])
+    #         count += 1
+    #     print("Please enter the number of your answer:")
+    #     userAnswer = raw_input()
+    #     while not userAnswer.isdigit() or not 0 < int(userAnswer) <= len(possible):
+    #         print("Try again - please enter a valid number:")
+    #         userAnswer = raw_input()
+    #     userAnswer = int(userAnswer)
+    #     if possible[userAnswer - 1] == qItem.correctAnswer:
+    #         print("Correct answer!")
+    #         score += 1
+    #     else:
+    #         print("Wrong answer")
+    #         print("Correct answer was: " + qItem.correctAnswer)
+    #         print("")
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
